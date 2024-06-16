fec3ad195e9c72ed7a3da7f7a8d6c7ad1b48254b
hamna noor <noorhamna174@gmail.com>
2024-06-16 16:26:21 +0500

    KnapSack Algo updated file

--- Week1/Day4_Task2.py
+++ Week1/Day4_Task2.py
@@ -1,27 +1,13 @@
-# A naive recursive implementation
-# of 0-1 Knapsack Problem
-
-# Returns the maximum value that
-# can be put in a knapsack of
-# capacity W
-
-
 def knapSack(capacity, wt, profit, n):
 
-    # Base Case
+
     if n == 0 or capacity == 0:
         return 0
 
-    # If weight of the nth item is
-    # more than Knapsack of capacity W,
-    # then this item cannot be included
-    # in the optimal solution
     if (wt[n-1] > capacity):
         return knapSack(capacity, wt, profit, n-1)
 
-    # return the maximum of two cases:
-    # (1) nth item included
-    # (2) not included
+
     else:
         return max(
             profit[n-1] + knapSack(
